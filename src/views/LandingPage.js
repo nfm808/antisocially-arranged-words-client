b68809ea6627185e28e8d21baa5c56dcf6776e3d
Nathan Fowler <luxurysandbox@gmail.com>
2019-07-22 01:19:22 -1000

    styling and editing of components

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero/Hero';
+import LearnMore from '../components/LearnMore/LearnMore';
+import './LandingPage.css';
 
 export default function LandingPage() {
+
   return (
-    <>
-      
-    </>
+    <section className='LandingPage'>
+      {/* <Hero 
+        heading='Antisocially Arranged Words'
+        subHeading='Bringing the horrible coffee table game across the web.'
+      />  */}
+      <h1>Antisocially Arranged Words</h1>
+      <h3>Dragging the horrible coffee table game across the web.</h3>
+      <LearnMore />     
+    </section>
   )
 }
