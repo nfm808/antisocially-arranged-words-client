50d24b002ce446c6be6510a956ea31f189da0e27
Nathan Fowler <luxurysandbox@gmail.com>
2019-07-23 16:04:46 -1000

    updated 404 message

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -5,7 +5,7 @@ export default function Error404(props) {
   return (
     <section className='Error404'>
       <button className="Error404--WhiteCard" type='button' onClick={() => props.history.goBack()}>
-        Go Back From Whence You Came!
+        Wait a Sec, you don't belong here! Go Back From Whence You Came! Click Me
       </button>
     </section>
   )
