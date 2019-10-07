f1536bf8835f866980a4af1f1a413afb7283c9f1
Nathan Fowler <luxurysandbox@gmail.com>
2019-10-07 12:28:32 -1000

    added temp

---
+++ temp.js
@@ -0,0 +1,10 @@
+const currentUser = {
+  isCzar: false,
+  user: user,
+  hand: userHand,
+  usedBlackCards: usedBlack,
+  usedWhiteCards: usedWhite,
+  answerChoices: answers,
+  currentBlackCard: adjusted,
+  players: players
+}
